e7cc6d0b65db7b33be6f64ebb887a09164535a0b
jganado <jganado@ryerson.ca>
2016-09-30 10:59:00 -0400

    Update OLMHash.c

--- OLMHash.c
+++ OLMHash.c
@@ -4,14 +4,17 @@
 
 int main(int argc,char* argv[])
 {
+	FILE * file;
 	char user[32];
 	int userValid = 0; 
 	int newUser = 0;
-	char password[12];  
+	char password[12]; 
+	int attempts = 0;
 
 
 /* User logging in */ 
-	while (userValid == 0)
+	userValid = login(newUser, user);
+/*	while (userValid == 0)
 	{
 		printf("Please enter a user id");
 		scanf("%31s",user); 
@@ -23,22 +26,56 @@ int main(int argc,char* argv[])
 				newUser = 1;
 			}
 		}
-	}
+	}*/
 /* New User Proccess */ 
 	if(userValid == 1 && newUser == 1)
 	{
-		printf("Please enter a new password(Max length of 12)"); 
-		scanf("%11s",password); 
+		newUserPass(password);
+	}	
+}
+
+int login(int newUser, char user){
+	printf("Please enter your User ID.");
+	scanf("%31s",user); 
+	if(strlen(user) => 4 && strlen(user) < 32)  
+	{
+		if (user name is not database)
+		{
+			newUser = 1;
+		}
+		return 1;
 	}
-	if(sizeof(password)<12)
+	else
+	{
+		return login(newUser, user);
+	}
+}
+
+int newUserPass(char password){
+	
+	int i;
+	
+	printf("Please enter a new password(Max length of 12)."); 
+	scanf("%11s",password);
+	
+	if(strlen(password) < 12)
 	{ 
-		for(var i=sizeof(password);i<12;i++)
+		for(i = strlen(password); i < 12 ;i++)
 		{
 			strcat(password,"\0");
 		}
 	}
-
-
-	
+	else if (strlen(password) > 12)
+	{
+		return newUserPass(password);
+	}
+	else if (strlen(password) == 12)
+	{
+		return 1;
+	}
 }
+
+
+
+
 	
